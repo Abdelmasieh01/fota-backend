0ddee39a042b66b08f5b91d5fd5e0963fc08f9cf
Abdelmasieh01 <gokuabdo31@gmail.com>
2024-01-11 22:55:28 +0200

    added firmware lines

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,37 @@
+from typing import Any
 from django.contrib import admin
-from .models import Firmware
+from .models import Firmware, FirmwareLine
+import os
+from django.conf import settings
 
 # Register your models here.
-admin.site.register(Firmware)
+
+
+class FirmwareAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version')
+
+    def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
+        super().save_model(request, obj, form, change)
+        lines = obj.file.open('r').read().splitlines()
+        # print(lines)
+        line_objects = FirmwareLine.objects.filter(firmware=obj)
+        if line_objects.count():
+            print(line_objects.delete())
+
+        counter = 0
+        line_objects = []
+        for line in lines:
+            if line.strip():
+                line_objects.append(FirmwareLine(
+                    firmware=obj, number=counter, line=line))
+                counter += 1
+
+        FirmwareLine.objects.bulk_create(line_objects)
+
+
+class FirmwareLineAdmin(admin.ModelAdmin):
+    list_display = ['firmware_name', 'number']
+
+
+admin.site.register(Firmware, FirmwareAdmin)
+admin.site.register(FirmwareLine, FirmwareLineAdmin)
--- main/models.py
+++ main/models.py
@@ -2,11 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+def file_path(instance, filename):
+    return f'firmwares/{instance.name}/{filename}'
+
 
 class Firmware(models.Model):
     name = models.CharField(max_length=50)
     version = models.CharField(unique=True, max_length=10)
-    file = models.FileField()
+    file = models.FileField(upload_to=file_path)
 
     def __str__(self):
-        return 'Firmware: ' + self.name + ' - ' + 'Version: ' + self.version
+        return self.name + ' - ' + 'Version: ' + self.version
+
+
+class FirmwareLine(models.Model):
+    firmware = models.ForeignKey(Firmware, on_delete=models.CASCADE)
+    number = models.PositiveSmallIntegerField()
+    line = models.CharField(max_length=200)
+
+    @property
+    def firmware_name(self):
+        return self.firmware.name
+
+    def __str__(self):
+        return f'Line Number: {self.number} of firmware: {self.firmware_name}'
